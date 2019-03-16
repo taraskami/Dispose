2c544f6176df683c75ea54eda372ba36ef97274f
OldManZhao <howardzhao99@aim.com>
2019-03-16 13:33:52 -0400

    hamburger

---
+++ js/hamburger.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+<div class="nav-toggle">
+    <div class="nav-toggle-bar"></div>
+</div>
+    <nav class="nav">
+        <ul>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
