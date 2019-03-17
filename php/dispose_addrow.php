877db709ddaf6927f2739655f94f665129af0e8c
taraskami <taras.kami@hotmail.com>
2019-03-16 23:33:41 -0400

    cosmetics

--- php/dispose_addrow.php
+++ php/dispose_addrow.php
@@ -10,6 +10,7 @@ $address = $_GET['address'];
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
 $type = $_GET['type'];
+$status = $_GET['status'];
 
 // Opens a connection to a MySQL server.
 $connection = new mysqli($servername, $username);
@@ -28,11 +29,12 @@ $address_sql = mysqli_real_escape_string($connection, $address);
 $lat_sql = mysqli_real_escape_string($connection, $lat);
 $lng_sql = mysqli_real_escape_string($connection, $lng);
 $type_sql = mysqli_real_escape_string($connection, $type);
+$status_sql = mysqli_real_escape_string($connection, $status);
 
 // Inserts new row with place data.
 $query = sprintf("INSERT INTO markers " .
-         " (id, name, address, lat, lng, type ) " .
-         " VALUES (NULL, '$name_sql', '$address_sql', '$lat_sql', '$lng_sql', '$type_sql');");
+         " (id, name, address, lat, lng, type, status) " .
+         " VALUES (NULL, '$name_sql', '$address_sql', '$lat_sql', '$lng_sql', '$type_sql', '$status_sql');");
 
 $result = mysqli_query($connection, $query);
 
